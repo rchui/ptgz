2d5cfe2dc9bc56207c5738678e5d48ccb3e16327
rchui <ryan.w.chui@gmail.com>
2017-04-22 15:29:11 -0500

    Implemented recursive file counting

--- src/ptxz.cpp
+++ src/ptxz.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <dirent.h>
 
 void helpCheck(char *argv[]) {
 	if (argv[1] == std::string("-h") || argv[1] == std::string("--help")) {
@@ -8,12 +9,42 @@ void helpCheck(char *argv[]) {
 	}
 }
 
+void findAll(int *numFiles, const char *cwd) {
+	DIR *dir;
+	struct dirent *ent;
 
+	// Check if cwd is a directory
+	if ((dir = opendir(cwd)) != NULL) {
+		// Get all file paths within directory.
+		while ((ent = readdir (dir)) != NULL) {
+			std::string fileBuff = std::string(ent -> d_name);
+			if (fileBuff != "." && fileBuff != "..") {
+				DIR *dir2;
+				std::string filePath = std::string(cwd) + "/" + fileBuff;
+				// Check if file path is a directory.
+				if ((dir2 = opendir(filePath.c_str())) != NULL) {
+					closedir(dir2);
+					findAll(numFiles, filePath.c_str());
+				} else {
+					*numFiles += 1;
+				}
+			}
+		}
+	}
+}
 
 char cwd [PATH_MAX];
 
 int main(int argc, char *argv[]) {
-	helpCheck(argv);
+	int *numFiles = new int(0);
+	
+	if (argc > 1) {
+		helpCheck(argv);
+	}
 	getcwd(cwd, PATH_MAX);
+	std::cout << cwd << std::endl;
+	findAll(numFiles, cwd);
+	
+	delete(numFiles);
 	return 0;
 }
