3076dedb7e7eaf7790e6b15775734261b33f8ca1
rchui <ryan.w.chui@gmail.com>
2017-04-22 15:06:53 -0500

    added initial helpCheck and saved cwd

--- src/ptxz.cpp
+++ src/ptxz.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <unistd.h>
+
+void helpCheck(char *argv[]) {
+	if (argv[1] == std::string("-h") || argv[1] == std::string("--help")) {
+		std::cout << "\nptxv - Parallel Tar XZ by Ryan Chui (2017)\n" << std::endl;
+		exit(0);
+	}
+}
+
+
+
+char cwd [PATH_MAX];
 
 int main(int argc, char *argv[]) {
-	
+	helpCheck(argv);
+	getcwd(cwd, PATH_MAX);
 	return 0;
 }
