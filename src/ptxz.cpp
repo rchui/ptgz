6dccfd38140950e8d897de10e1f1e7a4c247cc1f
rchui <ryan.w.chui@gmail.com>
2017-04-23 01:48:59 -0500

    added conditional so there won'rt be empty tar commands

--- src/ptxz.cpp
+++ src/ptxz.cpp
@@ -70,11 +70,13 @@ void compress(std::vector<std::string> *filePaths) {
 	#pragma omp parallel for schedule(dynamic)
 	for (int i = 0; i < omp_get_max_threads() * 100; ++i) {
 		unsigned long long int start = blockSize * i;
-		std::string command = "XZ_OPT=-9 tar cJf test." + std::to_string(i) + ".tar.xz ";
-		for (unsigned long long int j = start; j < std::min(start + blockSize, filePathSize); ++j) {
-			command += filePaths->at(j);
+		if (start < filePathSize) {
+			std::string command = "XZ_OPT=-9 tar cJf test." + std::to_string(i) + ".tar.xz ";
+			for (unsigned long long int j = start; j < std::min(start + blockSize, filePathSize); ++j) {
+				command += filePaths->at(j);
+			}
+			system(command.c_str());
 		}
-		system(command.c_str());
 	}
 
 }
