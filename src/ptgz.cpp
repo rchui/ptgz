8c8004c28c212fcf67f8e665f3c53400548c665d
rchui <ryan.w.chui@gmail.com>
2017-04-24 21:48:09 -0500

    stupid colon mistake

--- src/ptgz.cpp
+++ src/ptgz.cpp
@@ -208,7 +208,7 @@ void compression(std::vector<std::string> *filePaths, std::string name, bool ver
 			std::cout << "remove(" + rmCommand + ")\n";
 		}
 		if (!remove(rmCommand.c_str())) {
-			std::cout << "ERROR: " + rmCommand + " could not be removed."
+			std::cout << "ERROR: " + rmCommand + " could not be removed.";
 		}
 		
 
@@ -217,7 +217,7 @@ void compression(std::vector<std::string> *filePaths, std::string name, bool ver
 			std::cout << "remove(" + rmCommand + ")\n";
 		}
 		if (!remove(rmCommand.c_str())) {
-			std::cout << "ERROR: " + rmCommand + " coudl not be removed."
+			std::cout << "ERROR: " + rmCommand + " coudl not be removed.";
 		}
 	}
 
@@ -227,7 +227,7 @@ void compression(std::vector<std::string> *filePaths, std::string name, bool ver
 	}
 	rmCommand = name + ".ptgz.idx";
 	if (remove(rmCommand.c_str())) {
-		std::cout << "ERROR: " + rmCommand + " coudl not be removed."
+		std::cout << "ERROR: " + rmCommand + " coudl not be removed.";
 	}
 
 	tarNames->clear();
