0a4c5609b20ce9c026d6a6994dc4d35c523eb0cc
rchui <ryan.w.chui@gmail.com>
2017-05-02 16:04:09 -0500

    Limiting to 4 threads

--- src/ptgz.cpp
+++ src/ptgz.cpp
@@ -297,7 +297,7 @@ void extraction(std::vector<std::string> *filePaths, std::string name, bool verb
 	filePaths->pop_back();
 
 	// Unpack each tar.gz file
-	#pragma omp parallel for schedule(dynamic) num_threads(10)
+	#pragma omp parallel for schedule(dynamic) num_threads(4)
 	for (int i = 0; i < filePaths->size(); ++i) {
 		std::string gzCommand = "tar xzf " + filePaths->at(i);
 		if (verbose) {
