5dcf9c3c3ed673277eece8eecb11fb76f9a80636
rchui <ryan.w.chui@gmail.com>
2017-05-03 21:51:40 -0500

    Printing idx at weight

--- src/ptgz.cpp
+++ src/ptgz.cpp
@@ -319,6 +319,7 @@ void extraction(std::vector<std::string> *filePaths, std::string name, bool verb
 	std::cout << filePaths->size() << std::endl;
 	for (unsigned long long i = 0; i < filePaths->size(); ++i) {
 		weights->push_back(std::make_pair(GetFileSize(filePaths->at(i)), filePaths->at(i)));
+		std::cout << filePaths->at(i) << std::endl;
 	}
 	for (int i = 0; i < weights->size(); ++i) {
 		std::cout << weights->at(i).first << std::endl;
