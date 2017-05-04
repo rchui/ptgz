86d2b000a00b8e975caa7d990529525327551499
rchui <ryan.w.chui@gmail.com>
2017-05-03 22:20:06 -0500

    swapping to at from push back

--- src/ptgz.cpp
+++ src/ptgz.cpp
@@ -313,11 +313,11 @@ void extraction(std::vector<std::string> *filePaths, std::string name, bool verb
 	}
 	filePaths->pop_back();
 
-	std::vector<std::pair<unsigned long long, std::string>> *weights = new std::vector<std::pair<unsigned long long, std::string>>();
+	std::vector<std::pair<unsigned long long, std::string>> *weights = new std::vector<std::pair<unsigned long long, std::string>>(filePaths->size());
 
 	#pragma omp parallel for schedule(static)
 	for (unsigned long long i = 0; i < filePaths->size(); ++i) {
-		weights->push_back(std::make_pair(GetFileSize(filePaths->at(i)), filePaths->at(i)));
+		weights->at(i) = std::make_pair(GetFileSize(filePaths->at(i)), filePaths->at(i));
 	}
 	for (int i = 0; i < weights->size(); ++i) {
 		std::cout << weights->at(i).first << std::endl;
