3e1e0e45ea26b22536d9eb379aff45c7751dda5f
rchui <ryan.w.chui@gmail.com>
2017-05-15 16:01:16 -0500

    making system command call

--- src/ptgz-mpi.cpp
+++ src/ptgz-mpi.cpp
@@ -397,6 +397,7 @@ void extraction(std::string name, bool verbose, bool keep) {
 	for (uint64_t i = localBlock[0]; i < localBlock[0] + localBlock[1]; ++i) {
 		std::string tarCommand = "tar xf " + name + ".ptgz.tar " + std::to_string(i + localBlock[0]) + "." + name + "tar.gz";
 	}
+	system(tarCommand.c_str());
 	MPI_Finalize();
 	exit(0);
 
