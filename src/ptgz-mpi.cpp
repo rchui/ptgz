70f14d2afc73ffbdff0cc5c2226474f1a7da7fee
rchui <ryan.w.chui@gmail.com>
2017-05-18 02:20:23 -0500

    process printing for globalRank

--- src/ptgz-mpi.cpp
+++ src/ptgz-mpi.cpp
@@ -257,9 +257,9 @@ void compression(std::vector<std::string> *filePaths, std::string name, bool ver
 	for (int64_t i = localSize[0]; i < localSize[0] + localSize[1]; ++i) {
 		std::string gzCommand;
 		if (!levelSet) {
-			gzCommand = "tar -c -T " + std::to_string(i) + "." + name + ".ptgz.tmp | gzip -1 > " + std::to_string(i) + "." + name + ".tar.gz";
+			gzCommand = std::tostring(globalRank) + " tar -c -T " + std::to_string(i) + "." + name + ".ptgz.tmp | gzip -1 > " + std::to_string(i) + "." + name + ".tar.gz";
 		} else {
-			gzCommand = "tar -c -T " + std::to_string(i) + "." + name + ".ptgz.tmp | gzip -" + std::to_string(level) + " > " + std::to_string(i) + "." + name + ".tar.gz";
+			gzCommand = std::tostring(globalRank) + " tar -c -T " + std::to_string(i) + "." + name + ".ptgz.tmp | gzip -" + std::to_string(level) + " > " + std::to_string(i) + "." + name + ".tar.gz";
 		}
 		if (verbose) {
 			std::cout << gzCommand + "\n";
