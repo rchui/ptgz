f8218378d7c4ee964f717b756cc393797c06a789
rchui <ryan.w.chui@gmail.com>
2017-06-12 13:23:53 -0500

    commented out std::string

--- src/ptgz-mpi.cpp
+++ src/ptgz-mpi.cpp
@@ -583,7 +583,7 @@ void extraction(std::string name, bool verbose, bool keep) {
 	// Double check unpacking.
 	#pragma omp parallel for schedule(dynamic)
 	for (uint64_t i = 0; i < weights->size(); ++i) {
-		std::string gzCommand = "tar -x -k -z --null -f " + weights->at(i).second;
+		// std::string gzCommand = "tar -x -k -z --null -f " + weights->at(i).second;
 		char* const tarCommand[] = {
 									"tar",
 									"-x",
