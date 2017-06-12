4e4c91d04741ae2c20daf94a4cd75bf9576659ae
rchui <ryan.w.chui@gmail.com>
2017-06-12 12:03:31 -0500

    printing in one line

--- src/ptgz-mpi.cpp
+++ src/ptgz-mpi.cpp
@@ -349,9 +349,7 @@ void compression(std::vector<std::string> *filePaths, std::string name, bool ver
 									strToChar(std::to_string(i) + "." + name + ".ptgz.tar.gz"),
 									(char *) NULL
 								};
-		for (int i = 0; i < 7; ++i) {
-			printf("%s %s %s %s %s %s %s\n", gzCommand[0], gzCommand[1], gzCommand[2], gzCommand[3], gzCommand[4], gzCommand[5], gzCommand[6]);
-		}
+		printf("%s %s %s %s %s %s %s\n", gzCommand[0], gzCommand[1], gzCommand[2], gzCommand[3], gzCommand[4], gzCommand[5], gzCommand[6]);
 		if (verbose) {
 			// std::cout << gzCommand + "\n";
 		}
