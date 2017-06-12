c6886a960e6b76b7627b8ef7e56b2ba2f66a55af
rchui <ryan.w.chui@gmail.com>
2017-06-12 13:48:36 -0500

    delete array

--- src/ptgz-mpi.cpp
+++ src/ptgz-mpi.cpp
@@ -208,9 +208,9 @@ void makeScript(std::string name) {
 void clean(char *const input[]) {
 	size_t count = sizeof(input) / sizeof(input[0]);
 	for (int i = 0; i < count; ++i) {
-		delete input[i];
+		delete[] input[i];
 	}
-	delete [] input;
+	delete[] input;
 }
 
 char* strToChar(std::string input) {
