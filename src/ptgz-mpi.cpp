fb3d13b340ca1e613a73cfe061c3ab54c03fe972
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-21 15:12:56 -0500

    Added entire array

--- src/ptgz-mpi.cpp
+++ src/ptgz-mpi.cpp
@@ -452,7 +452,11 @@ void compression(std::vector<std::pair<uint64_t, std::string>> *filePaths, std::
 		// TODO:
 		char ** mpitarArray = new char * [6];
 		mpitarArray[0] = "mpitar";
-		// ;, "-c", "-f", strToChar(name + ".ptgz.tar"), "-T", strToChar(name + ".ptgz.tar")};
+		mpitarArray[1] = "-c";
+		mpitarArray[2] = "-f";
+		mpitarArray[3] = strToChar(name + ".ptgz.tar");
+		mpitarArray[4] = "-T";
+		mpitarArray[5] = strToChar(name + ".ptgz.tar");
 
 		mpitar(6, mpitarArray);
 		// execute(tarCommand);
