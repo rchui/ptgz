a5f487b0c7f2cea92270f8f40c8ca4583afdcd4d
Ryan Chui <ryan.w.chui@gmail.com>
2017-09-25 10:30:21 -0500

    Should be uint64_t for loop type

--- src/ptgz-mpi.cpp
+++ src/ptgz-mpi.cpp
@@ -730,7 +730,7 @@ int main(int argc, char *argv[]) {
 		std::vector<std::pair<uint64_t, std::string>> *filePaths = new std::vector<std::pair<uint64_t, std::string>>();
 		if (globalRank == root) {
 			getPaths(filePaths, cwd, "");
-			for (int_64_t i = 0; i < (*filePaths).size; i++) {
+			for (uint64_t i = 0; i < (*filePaths).size; i++) {
 				std::cout << (*filePaths).at(i) + "\n";
 			}
 			exit(0);
